f7f69de4abbfbdfecd22d8c5b1b9cfe635bf8202
theniaK <theniakoutrouli@gmail.com>
2018-03-16 20:07:21 +0200

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour 
+{
+    /// <summary>
+    /// Declare private varialbes
+    /// </summary>
+    private GameObject inventoryPanel;
+    private GameObject slotPanel;
+    private ItemDatabase database;
+    private int slotAmount;
+    /// <summary>
+    /// Declare public varialbes
+    /// </summary>
+    public GameObject inventorySlot;
+    public GameObject invetoryItem;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+    /// <summary>
+    /// Initialize variables
+    /// </summary>
+    void Start()
+    {
+        slotAmount = 20;
+        database = GetComponent<ItemDatabase>();
+        inventoryPanel = GameObject.Find("InventoryPanel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        for (int i = 0; i < slotAmount; i++)
+        {
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+            items.Add(new Item());
+        }
+    }
+    /// <summary>
+    /// A method that adds a slot in the inventory and an item in the slot
+    /// </summary>
+    /// <param name="id"></param>
+    public void AddItem(int id)
+    {
+        Item itemToAdd = database.FetchItemByID(id);
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObject = Instantiate(invetoryItem);
+                itemObject.transform.SetParent(slots[i].transform);
+                itemObject.transform.position = Vector2.zero; // Add the item in the center of the slot
+                itemObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObject.name = itemToAdd.Title;
+                break;
+            }
+        }
+    }
+}
---
+++ ItemDatabase.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour 
+{
+    /// <summary>
+    /// Declaration of private variables
+    /// </summary>
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+    /// <summary>
+    /// Initialization of variables
+    /// </summary>
+    void Start()
+    {
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+    }
+
+    /// <summary>
+    /// A method that runs the item list and finds the item with the wanted id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Item FetchItemByID(int id)
+    {
+        for (int i = 0; i < database.Count; i++)
+        {
+            if (database[i].ID == id)
+            {
+                return database[i];
+            }
+        }
+        return null;
+    }
+    /// <summary>
+    /// Add item data in the database
+    /// </summary>
+    void ConstructItemDatabase()
+    {
+        for (int i = 0; i < itemData.Count; i++)
+        {
+            database.Add(new Item((int)itemData[i]["id"] , itemData[i]["title"].ToString() , (int)itemData[i]["value"],
+                        (int)itemData[i]["stats"]["power"], (int)itemData[i]["stats"]["defence"], (int)itemData[i]["stats"]["vitality"],
+                        itemData[i]["stats"]["description"].ToString(), (bool)itemData[i]["stats"]["stackalbe"], (int)itemData[i]["stats"]["rapidity"]));
+        }
+    }
+}
+/// <summary>
+///Class Item
+/// </summary>
+public class Item
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public int Power { get; set; }
+    public int Defence { get; set; }
+    public int Vitality { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public int Rapidity { get; set; }
+    public Sprite Sprite { get; set; }
+
+    /// <summary>
+    /// If item not empty
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="title"></param>
+    /// <param name="value"></param>
+    /// <param name="power"></param>
+    /// <param name="defence"></param>
+    /// <param name="vitality"></param>
+    /// <param name="description"></param>
+    /// <param name="stackable"></param>
+    /// <param name="rapidity"></param>
+    public Item(int id , string title, int value ,int power, int defence, int vitality , string description, 
+                bool stackable , int rapidity) 
+    {
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Power = power;
+        this.Defence = defence;
+        this.Vitality = vitality;
+        this.Description = description;
+        this.Stackable = stackable;
+        this.Rapidity = rapidity;
+        this.Sprite = Resources.Load<Sprite>("Sprites/Sword");
+    }
+
+    /// <summary>
+    /// If the item is empty 
+    /// </summary>
+    public Item()
+    {
+        this.ID = -1;
+    }
+}
